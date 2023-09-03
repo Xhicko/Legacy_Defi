0771d9c43de1e51a9fcdedcab720aaafe6c7fe6d
Xhicko <otagadaisaac@gmail.com>
2023-09-03 17:49:13 +0100

    Developed the Service section

--- index.js
+++ index.js
@@ -19,3 +19,45 @@ function toggleMenu() {
     menuButton.classList.toggle('opened');
     menuButton.setAttribute('aria-expanded', menuButton.classList.contains('opened'));
   }
+
+
+  let liProjectAds = document.querySelector('.Li_Project_Advisory')
+  let liStraThin = document.querySelector('.Li_Strategic_Thinking')
+  let liTeamBui = document.querySelector('.Li_Team_Building')
+  let liStraThin2 = document.querySelector('.Li_Strategic_Thinking2')
+  let projectAds = document.querySelector('.Project_Advisory')
+  let StraThin = document.querySelector('.Strategic_Thinking')
+  let teamBui = document.querySelector('.Team_Building')
+  let StraThin2 = document.querySelector('.Strategic_Thinking2')
+
+  liProjectAds.addEventListener('click', ()=>{
+    projectAds.style.display = 'flex'
+    StraThin.style.display = 'none'
+    teamBui.style.display = 'none'
+    StraThin2.style.display = 'none'
+    projectAds.classList.add('slide-in')
+  })
+
+  liStraThin.addEventListener('click', ()=>{
+    projectAds.style.display = 'none'
+    StraThin.style.display = 'flex'
+    teamBui.style.display = 'none'
+    StraThin2.style.display = 'none'
+    StraThin.classList.add('slide-in')
+  })
+
+  liTeamBui.addEventListener('click', ()=>{
+    projectAds.style.display = 'none'
+    StraThin.style.display = 'none'
+    teamBui.style.display = 'flex'
+    StraThin2.style.display = 'none'
+    teamBui.classList.add('slide-in')
+  })
+
+  liStraThin2.addEventListener('click', ()=>{
+    projectAds.style.display = 'none'
+    StraThin.style.display = 'none'
+    teamBui.style.display = 'none'
+    StraThin2.style.display = 'flex'
+    StraThin2.classList.add('slide-in')
+  })
